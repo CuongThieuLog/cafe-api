b79e6e4350b907d51042c832d73cf3d92fa3f521
Cuong Thieu <dev.cuongtt@gmail.com>
2024-04-14 10:13:48 +0700

    update

--- src/routes/category.routes.js
+++ src/routes/category.routes.js
@@ -4,9 +4,9 @@ const auth = require("../middleware/auth.middleware");
 const roleAdmin = require("../middleware/admin.middleware");
 
 //private
-router.get("/", auth, CategoryController.getAll);
+router.get("/", CategoryController.getAll);
 router.post("/", auth, roleAdmin, CategoryController.create);
-router.get("/:id", auth, CategoryController.getById);
+router.get("/:id", CategoryController.getById);
 router.put("/:id", auth, roleAdmin, CategoryController.update);
 router.delete("/:id", auth, roleAdmin, CategoryController.delete);
 
--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -4,9 +4,9 @@ const auth = require("../middleware/auth.middleware");
 const roleAdmin = require("../middleware/admin.middleware");
 
 //private
-router.get("/", auth, ProductController.getAll);
+router.get("/", ProductController.getAll);
 router.post("/", auth, roleAdmin, ProductController.create);
-router.get("/:id", auth, ProductController.getById);
+router.get("/:id", ProductController.getById);
 router.put("/:id", roleAdmin, auth, ProductController.update);
 router.delete("/:id", auth, roleAdmin, ProductController.getById);
 router.get(
